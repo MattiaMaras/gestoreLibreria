db4b0788c9994a8178d5587c4f7be361e706ef5e
Mattia Marasco <mattiamarsco@gmail.com>
2025-08-28 16:56:22 +0200

    Aggiunta logica di redo nel CommandHandler

--- src/it/unical/gestorelibri/controller/CommandHandler.java
+++ src/it/unical/gestorelibri/controller/CommandHandler.java
@@ -5,19 +5,32 @@ import java.util.Stack;
 
 public class CommandHandler {
     private final Stack<Command> undoStack = new Stack<>();
-
+    private final Stack<Command> redoStack = new Stack<>();
 
     public void eseguiComando(Command command) {
         command.execute();
         undoStack.push(command);
+        redoStack.clear();
     }
 
     public void annullaUltimoComando() {
         if (!undoStack.isEmpty()) {
-            Command lastCommand = undoStack.pop();
-            lastCommand.undo();
+            Command command = undoStack.pop();
+            command.undo();
+            redoStack.push(command);
         } else {
             System.out.println("Nessuna operazione da annullare.");
+        }
+    }
+
+    public void ripristinaUltimoComando() {
+        if (!redoStack.isEmpty()) {
+            Command command = redoStack.pop();
+            command.execute();
+            // Rimetto il comando ripristinato nella pila dell'undo
+            undoStack.push(command);
+        } else {
+            System.out.println("Nessuna operazione da ripristinare.");
         }
     }
 }
